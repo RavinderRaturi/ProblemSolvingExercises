3a89e1f1a22356d2511d2d501aae5f1b4b49f691
Ravinder Raturi <ravinderraturi@hotmail.com>
2022-02-16 23:54:56 +0530

    Add project files.

---
+++ Solutions/Exercise1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingExercises
+{
+    static class Exercise1
+    {
+
+        static bool IsIntersectingLines(int[] directions)
+        {
+            //ToDo
+            return true;
+        }
+    }
+}
---
+++ Solutions/Exercise2.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolvingExercises
+{
+    static class Exercise2
+    {
+
+        static String GetLongestString(String Characters, String[] strings)
+        {
+            //ToDo
+            return "";
+        }
+    }
+}
